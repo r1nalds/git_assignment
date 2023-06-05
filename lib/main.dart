77846288a4a94865c7e7ce6ea85adf3dc73e8f40
r1nalds <r1naldeenc@gmail.com>
2023-06-05 21:59:42 +0300

    First changes

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
-        ),
+            child: Column(
+          children: const [Text("First change in my app"), Text("Stacked text")],
+        )),
       ),
     );
   }
