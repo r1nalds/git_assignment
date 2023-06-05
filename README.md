cccbffc1d356bcb7a150b4e836b7807214ce42cb
r1nalds <r1naldeenc@gmail.com>
2023-06-05 21:48:39 +0300

    Repository initialization

---
+++ README.md
@@ -0,0 +1,3 @@
+# git_assignment
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/git_assignment/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.git_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
